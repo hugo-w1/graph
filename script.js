105828f60acc97f5c23eb979428ba62cdda88739
hugo-w1 <hugo.wakt@elev.ga.lbs.se>
2024-04-19 12:55:31 +0200

    adjusted graph scaling

--- script.js
+++ script.js
@@ -1,37 +1,6 @@
 let cvs = document.getElementById('cvs');
 let ctx = cvs.getContext('2d');
-ctx.font = "20px Arial";
-/*
-let data = [
-    {
-        count: 1615,
-        year: '2017'
-    },
-    {
-        count: 1688,
-        year: '2018'
-    },
-    {
-        count: 1644,
-        year: '2019'
-    },
-    {
-        count: 1665,
-        year: '2020'
-    },
-    {
-        count: 1651,
-        year: '2021'
-    },
-    {
-        count: 1526,
-        year: '2022'
-    },
-    {
-        count: 1543,
-        year: '2023'
-    }
-];*/
+ctx.font = "10px Arial";
 
 
 let data = [];
@@ -39,13 +8,13 @@ let data = [];
 loadFile();
 async function loadFile() {
     //fetch file
-    await fetch('ME0104B8_20240417-010512.json')
+    await fetch('BE0101B8_20240419-124124.json')
         .then(response => response.json())
         .then(response => {
             response.data.forEach(element => {
                 data.push({
                     count: parseInt(element.values[0]),
-                    year: element.key[1]
+                    year: element.key[0]
                 });
             });
         });
@@ -56,7 +25,7 @@ async function loadFile() {
 }
 
 
-let maxXCount = () => {
+let maxYCount = () => {
     let max = 0;
     for (let i = 0; i < data.length; i++) {
         if (data[i].count > max) {
@@ -69,39 +38,27 @@ let maxXCount = () => {
 
 function drawGraph() {
 
-    //PRINT INFORMATION
-
-    //x 
-
-    let width = cvs.width / (data.length + 1);
-    let x = width;
-    for (let i = 0; i < data.length; i++) {
-        if (i % 1 == 0) {
-            console.log(data[i].year);
-            ctx.fillText(data[i].year, x, 380);
-        }
-        x += width
-    }
-
-
     let positions = calculateGraph();
 
     for (let i = 0; i < positions.length; i++) {
         ctx.beginPath();
         if (i == 0) {
-            //  ctx.moveTo(0, 400);
             ctx.lineTo(positions[i].x, positions[i].y);
+            console.log(positions[i].x, positions[i].y);
         } else {
             ctx.moveTo(positions[i - 1].x, positions[i - 1].y);
             ctx.lineTo(positions[i].x, positions[i].y);
-            if (i % 1 == 0) {
-                ctx.fillText(data[i].count, positions[i].x, positions[i].y);
+        }
+        if (i % 1 == 0) {
 
-            }
+            ctx.moveTo(positions[i].x, positions[i].y);
+            ctx.lineTo(positions[i].x, 380);
+
+            ctx.fillText(data[i].count, positions[i].x, positions[i].y);
+            ctx.fillText(data[i].year, positions[i].x, 380);
+
         }
         ctx.stroke();
-
-        // ctx.fillText(data[i].year, positions[i].x, positions[i].y);
     }
 }
 
@@ -109,21 +66,24 @@ function calculateGraph() {
     let positions = [];
 
     //canvas scale 
-    let x = cvs.width / (data.length);
-    let yScale = (cvs.height / maxXCount());
+    let xScale = cvs.width / (data.length + 3);
+    let yScale = cvs.height / (maxYCount() + 1);
+
+    console.log(xScale);
+    console.log(yScale);
 
     //starting pos  
-    let canvasX = x;
-    let canvasY = cvs.height + 50;
+    let canvasX = xScale;
+    let canvasY = cvs.height + 70;
 
     for (let i = 0; i < data.length; i++) {
 
         let xCoord = canvasX;
-        let yCoord = canvasY - data[i].count * yScale
+        let yCoord = canvasY - data[i].count * yScale;
 
         positions.push({ x: xCoord, y: yCoord });
 
-        canvasX += x;
+        canvasX += xScale;
     }
 
     console.log(positions);
